c4c42ba884fba2535e21c313cba3d5be2c701570
bodanakos <bodanakos@gmail.com>
2022-07-12 15:26:42 +0200

    added timeout check for api, date for returns

--- functions/breached.php
+++ functions/breached.php
@@ -1,19 +1,27 @@
 <?php
 
 function CheckIfPasswordIsBreached($password_input, $is_console) {
+	date_default_timezone_set("Europe/Budapest");
 	$password_format = strtoupper(sha1($password_input));
 	$password_sha1_prefix = substr($password_format, 0, 5);
 	$url_for_api = "https://api.pwnedpasswords.com/range/$password_sha1_prefix";
 	$password_sha1_without_prefix= substr($password_format, 5, 40);
 
 
-	$return_value = "Given password: ". $password_input . "<br>";
+	$return_value = date("H:i:s") . "[INFO]: Given password: ". $password_input . "<br>";
 	$return_value .= " > Password in SHA1 with uppercase: ". $password_format . "<br>";
 	$return_value .= " > Prefix: ". $password_sha1_prefix . "<br>";
 	$return_value .= " > Password without prefix: ". $password_sha1_without_prefix . "<br>";
 
-
-	$response = file_get_contents($url_for_api);
+	$options = stream_context_create(array('http'=>
+	    array(
+		    'timeout' => 10 // 10 seconds
+		 )
+	));
+	$response = file_get_contents($url_for_api, false, $options);
+	if (empty($response)) {
+		die (date("H:i:s") . ' [ERROR]: Cannot reach ' . $url_for_api . ' -> Connection Timed Out.');
+	}
 	$resp_to_array = explode("\n", $response);
 
 
